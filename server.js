1be4c923f169d8be8b7fc3c9189a79841f3837c2
wolf <hassan.yusry@gmail.com>
2018-06-28 14:52:30 +0200

    added ejs

--- server.js
+++ server.js
@@ -4,28 +4,52 @@ const bodyParser= require('body-parser')
  
 const MongoClient = require('mongodb').MongoClient
 const dburl= 'mongodb://mongouser:zew0lfen@ds121251.mlab.com:21251/mongz'
+var db
 
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 
 
 
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
+})
 // app.get('/', (req, res) => {
 //     res.send('Hello World')
 //   }) this send a responce back to the browser
 
-app.get('/', (req, res)=>{
-    res.sendfile(__dirname+'/index.html')
-})
+// app.get('/', (req, res)=>{
+//     // res.sendFile(__dirname+'/index.html')
+//     db.collection('quotes').find().toArray(function(err, results) {
+//       console.log(results)
+//       // send HTML file populated with quotes here
+//     })
+      
+  
+// })
+// app.get('/', (req, res) => {
+//   var cursor = db.collection('quotes').find()
+// })
+
 app.post('/quotes', (req,res)=>{
-console.log(req.body)
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
- 
 
 MongoClient.connect(dburl, (err, mongz) => {
   if (err) return console.log(err)
-  var db = mongz.db('mongz') // whatever your database name is
-  app.listen(3000, () => {
-    console.log('listening on 3000')
+  db = mongz.db('mongz') // whatever your database name is
+  app.listen(8000, () => {
+    console.log('listening on 8000')
 })
 })
 
